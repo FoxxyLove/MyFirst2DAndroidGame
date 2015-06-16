08598e2cf42dfb4a53598b16c33fd34a4ca7497f
FoxxyLove <ladyoli14225@gmail.com>
2015-06-16 09:24:36 +0200

    files

---
+++ app/src/main/java/com/example/kinia/myfirst2dgame/GamePanel.java
@@ -0,0 +1,84 @@
+package com.example.kinia.myfirst2dgame;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by kinia on 15.06.15.
+ */
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
+{
+    public static final int WIDTH = 856;
+    public static final int HEIGH = 480;
+    private MainThread thread;
+    private Background bg;
+
+    public GamePanel(Context context)
+    {
+        super(context);
+
+        getHolder().addCallback(this);
+        thread = new MainThread(getHolder(), this);
+
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int heigh){}
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
+        boolean retry = true;
+        while(retry)
+        {
+            try
+            {
+                thread.setRunning(false);
+                thread.join();
+            }
+            catch (InterruptedException e){e.printStackTrace();}
+            retry = false;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        bg = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.grassbg1));
+        bg.setVector(-5);
+        // start the game loop
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return super.onTouchEvent(event);
+    }
+
+    public void update()
+    {
+        bg.update();
+    }
+
+    @Override
+    public void draw(Canvas canvas)
+    {
+        final float scaleFactorX = getWidth()/WIDTH;
+        final float scaleFactorY = getHeight()/HEIGH;
+        if (canvas!=null) {
+            final int savedState = canvas.save();
+            canvas.scale(scaleFactorX, scaleFactorY);
+            bg.draw(canvas);
+            canvas.restoreToCount(savedState);
+        }
+    }
+
+}
